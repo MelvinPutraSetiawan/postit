9ae99ba59cb1b83d2b6429e1651e69bcdf7b4320
Melvin <kirigayakazzeno@gmail.com>
2024-10-14 11:28:08 +0700

    Update Posts.tsx
    On going

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import PostCard from "./PostCard";
+
+const PostCardList = ({ data, handleTagClick }) => {
+  return <div className="mt-16 prompt_layout"></div>;
+};
 
 function Posts() {
-  return <div>Posts</div>;
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = () => {};
+
+  return (
+    <section className="feed">
+      <form action="" className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+      <PostCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  );
 }
 
 export default Posts;
