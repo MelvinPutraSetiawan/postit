01858ae1dcab0847426b856e8bdc77593e1eed90
Melvin <kirigayakazzeno@gmail.com>
2024-10-15 08:48:20 +0700

    Update route.tsx

--- src/app/api/post/route.tsx
+++ src/app/api/post/route.tsx
@@ -1,28 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
 import Post from "@/models/post";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import User from "@/models/user";
 import { connectToDB } from "@/utils/database";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const GET = async (request: NextRequest): Promise<NextResponse> => {
+export const GET = async (): Promise<Response> => {
   try {
     await connectToDB();
-
     const posts = await Post.find({}).populate("creator");
 
-    return new NextResponse(JSON.stringify(posts), {
+    return new Response(JSON.stringify(posts), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
         "Cache-Control":
           "no-store, no-cache, must-revalidate, proxy-revalidate",
-        Pragma: "no-cache",
-        Expires: "0",
       },
     });
   } catch (error) {
-    console.error("Error fetching posts:", error);
-    return new NextResponse(`Error: ${error}`, { status: 500 });
+    return new Response(`Error: ${error}`, { status: 500 });
   }
 };
