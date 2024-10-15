7f50fbd16d732d932bf6adf43e3ee473c1b6568d
Melvin <kirigayakazzeno@gmail.com>
2024-10-15 07:55:15 +0700

    this version work in build

--- src/app/api/post/route.tsx
+++ src/app/api/post/route.tsx
@@ -1,25 +1,21 @@
-import { NextResponse } from "next/server";
 import Post from "@/models/post";
 import { connectToDB } from "@/utils/database";
 
-export const GET = async (): Promise<NextResponse> => {
+export const GET = async (): Promise<Response> => {
   try {
     await connectToDB();
-
     const posts = await Post.find({}).populate("creator");
 
-    return new NextResponse(JSON.stringify(posts), {
+    return new Response(JSON.stringify(posts), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
-        "Cache-Control":
-          "no-store, no-cache, must-revalidate, proxy-revalidate",
+        "Cache-Control": "no-cache, no-store, must-revalidate, max-age=0",
         Pragma: "no-cache",
         Expires: "0",
       },
     });
   } catch (error) {
-    console.error("Error fetching posts:", error);
-    return new NextResponse(`Error: ${error}`, { status: 500 });
+    return new Response(`Error: ${error}`, { status: 500 });
   }
 };
