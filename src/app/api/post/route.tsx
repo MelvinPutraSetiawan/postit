04235e3366d84b9845644514ef8cc0ec45dc5e06
Melvin <kirigayakazzeno@gmail.com>
2024-10-15 08:40:24 +0700

    Update route.tsx

--- src/app/api/post/route.tsx
+++ src/app/api/post/route.tsx
@@ -1,20 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
 import Post from "@/models/post";
+import User from "@/models/user";
 import { connectToDB } from "@/utils/database";
 
-export const GET = async (): Promise<Response> => {
+export const GET = async (request: NextRequest): Promise<NextResponse> => {
   try {
     await connectToDB();
+
     const posts = await Post.find({}).populate("creator");
 
-    return new Response(JSON.stringify(posts), {
+    return new NextResponse(JSON.stringify(posts), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
         "Cache-Control":
           "no-store, no-cache, must-revalidate, proxy-revalidate",
+        Pragma: "no-cache",
+        Expires: "0",
       },
     });
   } catch (error) {
-    return new Response(`Error: ${error}`, { status: 500 });
+    console.error("Error fetching posts:", error);
+    return new NextResponse(`Error: ${error}`, { status: 500 });
   }
 };
